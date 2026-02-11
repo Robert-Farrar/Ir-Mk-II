8fd82ccff82e1ad9256000f922aa77ccead97119
Robert-Farrar <157628762+Robert-Farrar@users.noreply.github.com>
2026-02-10 20:57:10 -0500

    Delete PmiPair class
    Remove PmiPair class and its constructor.

--- Ir/PmiPair.java
+++ Ir/PmiPair.java
@@ -1,10 +1,1 @@
-public class PmiPair {
-    String w1,w2;
-    double score;
 
-    PmiPair(String w1, String w2, double score) {
-        this.w1 = w1;
-        this.w2 = w2;
-        this.score = score;
-    }
-}
